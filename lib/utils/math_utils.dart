649651be319fe14ac9be04373f7875d0d03909ce
Adil Basar <descrout@gmail.com>
2024-04-01 21:57:59 +0300

    math utils static

--- lib/utils/math_utils.dart
+++ lib/utils/math_utils.dart
@@ -1,21 +1,21 @@
 class MathUtils {
-  double lerp(double a, double b, double t) {
+  static double lerp(double a, double b, double t) {
     return a + (b - a) * t;
   }
 
-  double inverseLerp(double a, double b, double v) {
+  static double inverseLerp(double a, double b, double v) {
     return (v - a) / (b - a);
   }
 
-  double remap(double v, double a, double b, double c, double d) {
+  static double remap(double v, double a, double b, double c, double d) {
     return lerp(c, d, inverseLerp(a, b, v));
   }
 
-  double constrain(double val, double min, double max) {
+  static double constrain(double val, double min, double max) {
     if (val < min) return min;
     if (val > max) return max;
     return val;
   }
 
-  int lcm(int a, int b) => (a * b) ~/ a.gcd(b);
+  static int lcm(int a, int b) => (a * b) ~/ a.gcd(b);
 }
