67cefc062a69c16ff3c8edbc17b17be7379140e5
Adil Basar <descrout@gmail.com>
2024-04-12 16:01:54 +0300

    random math utilities added

--- lib/utils/math_utils.dart
+++ lib/utils/math_utils.dart
@@ -1,4 +1,32 @@
+import 'dart:math';
+
 class MathUtils {
+  static Random _rnd = Random();
+
+  static int randInt(int min, int max) {
+    return _rnd.nextInt(max - min) + min;
+  }
+
+  static double randDouble(double min, double max) {
+    return _rnd.nextDouble() * (max - min) + min;
+  }
+
+  static T randPick<T>(List<T> arr) {
+    return arr[_rnd.nextInt(arr.length)];
+  }
+
+  static bool flipCoin() {
+    return _rnd.nextBool();
+  }
+
+  static void shuffle<T>(List<T> arr) {
+    arr.shuffle(_rnd);
+  }
+
+  static void seedRandom(int seed) {
+    _rnd = Random(seed);
+  }
+
   static double lerp(double a, double b, double t) {
     return a + (b - a) * t;
   }
