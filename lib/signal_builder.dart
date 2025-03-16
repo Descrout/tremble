56321d059a73e0633dc7e00b6442acb081d0810d
Adil Basar <descrout@gmail.com>
2025-03-17 00:47:41 +0300

    signal builders added

--- lib/signal_builder.dart
+++ lib/signal_builder.dart
@@ -7,7 +7,12 @@ class SignalBuilder<T> extends StatefulWidget {
       {super.key, required this.builder, required this.signal, required this.onSignal});
 
   final Signal<T> signal;
+
+  /// If you return true it will rebuild the widget
+  /// If you return false it won't rebuild the widget, and it will stop listening to the signal
+  /// If you return null it won't rebuild the widget, but it will still listen to the signal
   final SubscriptionCallback<T> onSignal;
+
   final Widget Function(BuildContext context, T? value) builder;
 
   @override
@@ -46,5 +51,47 @@ class _SignalBuilderState<T> extends State<SignalBuilder<T>> {
   @override
   Widget build(BuildContext context) {
     return widget.builder(context, stateValue);
+  }
+}
+
+class SignalsBuilder extends StatefulWidget {
+  const SignalsBuilder(
+      {super.key, required this.builder, required this.signals, required this.onSignal});
+
+  final List<Signal> signals;
+  final VoidCallback onSignal;
+  final Widget Function(BuildContext context) builder;
+
+  @override
+  State<SignalsBuilder> createState() => _SignalsBuilderState();
+}
+
+class _SignalsBuilderState extends State<SignalsBuilder> {
+  bool? onSignal(value) {
+    widget.onSignal();
+    setState(() {});
+    return true;
+  }
+
+  @override
+  void initState() {
+    for (final signal in widget.signals) {
+      signal.listen(onSignal);
+    }
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    for (final signal in widget.signals) {
+      signal.unlisten(onSignal);
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder(context);
   }
 }
