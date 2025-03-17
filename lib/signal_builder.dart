4152f7a02b923830cd7bb6ca274416fac1ae6708
Adil Basar <descrout@gmail.com>
2025-03-17 15:48:35 +0300

    onsignal callbacks made optional

--- lib/signal_builder.dart
+++ lib/signal_builder.dart
@@ -3,15 +3,14 @@ import 'package:tremble/signal.dart';
 import 'package:tremble/utils/types.dart';
 
 class SignalBuilder<T> extends StatefulWidget {
-  const SignalBuilder(
-      {super.key, required this.builder, required this.signal, required this.onSignal});
+  const SignalBuilder({super.key, required this.builder, required this.signal, this.onSignal});
 
   final Signal<T> signal;
 
   /// If you return true it will rebuild the widget
   /// If you return false it won't rebuild the widget, and it will stop listening to the signal
   /// If you return null it won't rebuild the widget, but it will still listen to the signal
-  final SubscriptionCallback<T> onSignal;
+  final SubscriptionCallback<T>? onSignal;
 
   final Widget Function(BuildContext context, T? value) builder;
 
@@ -23,7 +22,7 @@ class _SignalBuilderState<T> extends State<SignalBuilder<T>> {
   T? stateValue;
 
   bool? onSignal(T value) {
-    final result = widget.onSignal(value);
+    final result = widget.onSignal?.call(value);
 
     if (result == false) {
       return false;
@@ -55,11 +54,10 @@ class _SignalBuilderState<T> extends State<SignalBuilder<T>> {
 }
 
 class SignalsBuilder extends StatefulWidget {
-  const SignalsBuilder(
-      {super.key, required this.builder, required this.signals, required this.onSignal});
+  const SignalsBuilder({super.key, required this.builder, required this.signals, this.onSignal});
 
   final List<Signal> signals;
-  final VoidCallback onSignal;
+  final VoidCallback? onSignal;
   final Widget Function(BuildContext context) builder;
 
   @override
@@ -68,7 +66,7 @@ class SignalsBuilder extends StatefulWidget {
 
 class _SignalsBuilderState extends State<SignalsBuilder> {
   bool? onSignal(value) {
-    widget.onSignal();
+    widget.onSignal?.call();
     setState(() {});
     return true;
   }
